c7c2e7d83573e44b6ac30b5d00cfcf723a0c8593
Lee <9787206@qq.com>
2021-08-23 21:20:34 +0800

    对象重写此方法，GC回收时 会执行

--- demo1/M.java
+++ demo1/M.java
@@ -5,6 +5,10 @@ package threadlocal.demo;
  */
 public class M {
 
+    /**
+     * 对象重写此方法，GC回收时 会执行
+     * @throws Throwable
+     */
     @Override
     protected void finalize() throws Throwable {
         System.out.println("finalize");
